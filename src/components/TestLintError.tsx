07f2db5c0e6c1d4faf28ec7d83bbbfd8fb8f4fa2
Oluwadurotimi Mejabi <durotimitech@gmail.com>
2025-07-06 16:45:20 +0100

    added hero section to landing page

--- src/components/TestLintError.tsx
+++ src/components/TestLintError.tsx
@@ -1,6 +1,3 @@
 import React from 'react';
-import Unused from './Hero'; // This import is unused and should trigger the lint rule
-
-export default function TestLintError() {
-    return <div>Test Lint Error</div>;
-} 
+import Unused from './Hero';
+export default function TestLintError() { return <div>Test Lint Error</div>; }
