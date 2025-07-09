22429c5973d221442cc3c67f0eeb89d3e20f9e78
timmymejabitoast <timmy.mejabi@toasttab.com>
2025-07-09 10:45:23 +0100

    Fixed build errors

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,6 +13,7 @@ export default async function BlogPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {posts.map((post) => (
             <BlogPostCard key={post.slug} {...post} />
+            
           ))}
         </div>
       </div>
