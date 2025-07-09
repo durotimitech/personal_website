ab010960b1bad9c29502f4392884f58799d1e4c8
timmymejabitoast <timmy.mejabi@toasttab.com>
2025-07-09 15:51:39 +0100

    removed testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
-// import EditorialSection from "@/components/EditorialSection";
+import EditorialSection from "@/components/EditorialSection";
 
 export default function Home() {
   return (
     <main>
       <AboutSection />
       <ServicesSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       {/* <EditorialSection /> */}
     </main>
   );
